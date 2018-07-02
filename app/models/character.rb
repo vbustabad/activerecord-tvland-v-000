3e45e74f86ff93af99f4f715d20d5492ae5d422c
Veronica Bustabad <vbustabad@gmail.com>
2018-07-02 23:33:51 +0000

    automatically backed up by learn

---
+++ app/models/character.rb
@@ -0,0 +1,12 @@
+class Character < ActiveRecord::Base
+
+  belongs_to :actor
+  belongs_to :show
+
+  def say_that_thing_you_say
+    "#{character.name} always says: #{character.catchphrase}"
+  end 
+
+end 
+
+  
